e6740a2cffd417a1e64c9e2e9e0e56d1231a0263
Pavel Ershov <devnulpavel@gmail.com>
2016-10-16 18:34:39 +0300

    New commentaries 2

--- main.cpp
+++ main.cpp
@@ -201,63 +201,52 @@ int multithreadedServer() {
 
 
 int tcpServer() {
-    int port = 5555;
-    if(port < 0 || port > 65535 ){
-        fprintf( stderr, "Задан некорректный номер порта.\n" );
-        return -1;
-    }
-    
-    event_base* base = event_base_new();
-    if(!base ){
-        fprintf( stderr, "Ошибка при создании объекта event_base.\n" );
-        return -1;
-    }
-    
-    // адресс
-    sockaddr_in sin;
-    memset( &sin, 0, sizeof(sin) );
-    sin.sin_family = AF_INET;    /* работа с доменом IP-адресов */
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* принимать запросы с любых адресов */
-    sin.sin_port = htons(port);
-    
-    
+    //////////////////////////////////////////////////
+    // Callbacks
+    //////////////////////////////////////////////////
     // обработка принятия соединения
     auto accept_connection_cb = [](evconnlistener *listener,
                                    evutil_socket_t fd, sockaddr *addr, int sock_len,
                                    void* arg) {
         
-        // При обработке запроса нового соединения необходимо создать для него объект bufferevent
+        // обработчик ивентов базовый
         event_base* base = evconnlistener_get_base(listener);
+        
+        // При обработке запроса нового соединения необходимо создать для него объект bufferevent
         bufferevent* buf_ev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE );
         
         // Функция обратного вызова для события: данные готовы для чтения в buf_ev
-        auto echo_read_cb = [](bufferevent *buf_ev, void *arg) {
+        auto echo_read_cb = [](bufferevent* buf_ev, void *arg) {
             
             evbuffer* buf_input = bufferevent_get_input(buf_ev);
             evbuffer* buf_output = bufferevent_get_output(buf_ev);
+            
+            // тестовая задержка
+            //std::this_thread::sleep_for(std::chrono::milliseconds(40));
             
             // Данные просто копируются из буфера ввода в буфер вывода
             evbuffer_add_printf(buf_output, "You write: ");
             evbuffer_add_buffer(buf_output, buf_input);
         };
         
-        // Функция обратного вызова для события: данные готовы для чтения в buf_ev
-        auto echo_write_cb = [](bufferevent *buf_ev, void *arg) {
-            
+        // Функция обратного вызова для события: данные готовы для записи в buf_ev
+        auto echo_write_cb = [](bufferevent* buf_ev, void *arg) {
+            //std::cout << "Write callback" << std::endl;
         };
         
         // коллбек обработки ивента
-        auto echo_event_cb = []( struct bufferevent *buf_ev, short events, void *arg){
-            if( events & BEV_EVENT_ERROR ){
-                perror( "Ошибка объекта bufferevent" );
+        auto echo_event_cb = [](bufferevent* buf_ev, short events, void *arg){
+            if(events & BEV_EVENT_ERROR ){
+                perror("Ошибка объекта bufferevent");
             }
-            if( events & (BEV_EVENT_EOF | BEV_EVENT_ERROR) ){
-                bufferevent_free( buf_ev );
+            if(events & (BEV_EVENT_EOF | BEV_EVENT_ERROR)){
+                bufferevent_free(buf_ev);
             }
         };
         
+        // коллбеки обработи
         bufferevent_setcb(buf_ev, echo_read_cb, echo_write_cb, echo_event_cb, NULL);
-        bufferevent_enable(buf_ev, (EV_READ | EV_WRITE) );
+        bufferevent_enable(buf_ev, (EV_READ | EV_WRITE));
     };
 
     // ошибка в принятии соединения
@@ -266,16 +255,51 @@ int tcpServer() {
         int error = EVUTIL_SOCKET_ERROR();
         fprintf( stderr, "Ошибка %d (%s) в мониторе соединений. Завершение работы.\n",
                 error, evutil_socket_error_to_string( error ) );
-        event_base_loopexit( base, NULL );
+        event_base_loopexit(base, NULL);
+    };
+    
+    // коллбек таймаута чтения
+    auto updateEvent = [](evutil_socket_t socketFd, short event, void *arg){
+        if (event & EV_TIMEOUT) {
+            std::cout << "Таймаут события" << std::endl;
+        } else if (event & EV_READ) {
+            std::cout << "Таймаут EV_READ" << std::endl;
+        } else if (event & EV_PERSIST) {
+            std::cout << "Таймаут PERSIST" << std::endl;
+        }
     };
 
+    //////////////////////////////////////////////////
+    // setup
+    //////////////////////////////////////////////////
+    // обработчик событий
+    event_base* base = event_base_new();
+    if(!base){
+        fprintf( stderr, "Ошибка при создании объекта event_base.\n" );
+        return -1;
+    }
+    
+    // коллбек-ивент для периодических событий
+    timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    event* updateEventObject = event_new(base, fileno(stdin), EV_TIMEOUT | EV_READ | EV_PERSIST, updateEvent, NULL);
+    event_add(updateEventObject, &tv);
+    
+    // адрес
+    const int port = 5555;
+    sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;    /* работа с доменом IP-адресов */
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* принимать запросы с любых адресов */
+    sin.sin_port = htons(port);
     
     // лиснер
     evconnlistener* listener = evconnlistener_new_bind(base, accept_connection_cb, NULL,
                                        (LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE),
                                        -1, (sockaddr*)&sin, sizeof(sin));
-    // проверка ошибки
-    if( !listener ){
+    // проверка ошибки создание листнера
+    if(!listener){
         perror( "Ошибка при создании объекта evconnlistener" );
         return -1;
     }
@@ -285,6 +309,11 @@ int tcpServer() {
     
     // запуск обработки событий
     event_base_dispatch(base);
+    
+    // delete all
+    event_free(updateEventObject);
+    evconnlistener_free(listener);
+    event_base_free(base);
     
     return 0;
 }
