e868ab2a98641b1e1f8ebfbf72e97f14d57903ec
Pavel Ershov <devnulpavel@gmail.com>
2016-10-16 15:47:04 +0300

    New commentaries

--- main.cpp
+++ main.cpp
@@ -5,117 +5,171 @@
 #include <thread>
 #include <cstdint>
 #include <vector>
+// libevent
 #include <evhttp.h>
 
 // пример
 // https://habrahabr.ru/post/217437/
 
+using namespace std;
+
+typedef unique_ptr<evhttp, decltype(&evhttp_free)> ServerPtr;
 
 int simpleOneThreadServer(){
+    // Инициализация цикла
     if (!event_init()) {
         std::cerr << "Failed to init libevent." << std::endl;
         return -1;
     }
     
-    char const SrvAddress[] = "127.0.0.1";
-    std::uint16_t SrvPort = 5555;
-    std::unique_ptr<evhttp, decltype(&evhttp_free)> Server(evhttp_start(SrvAddress, SrvPort), &evhttp_free);
-    if (!Server)
-    {
+    // создаем сервер
+    char const* serverAddress = "127.0.0.1";
+    uint16_t serverPort = 5555;
+    
+    // сервер + функция, вызываемая при уничтожении
+    ServerPtr server(evhttp_start(serverAddress, serverPort), &evhttp_free);
+    
+    // не удалось создать сервер
+    if (!server) {
         std::cerr << "Failed to init http server." << std::endl;
         return -1;
     }
-    void (*OnReq)(evhttp_request *req, void *) = [] (evhttp_request *req, void *)
-    {
-        auto *OutBuf = evhttp_request_get_output_buffer(req);
-        if (!OutBuf)
+    
+    // коллбек запроса
+    void (*receivedRequest)(evhttp_request *req, void *) = [](evhttp_request *req, void *){
+        // выходной буффер запроса
+        auto* outBuf = evhttp_request_get_output_buffer(req);
+        if (!outBuf){
             return;
-        evbuffer_add_printf(OutBuf, "<html><body><center><h1>Hello Wotld!</h1></center></body></html>");
-        evhttp_send_reply(req, HTTP_OK, "", OutBuf);
+        }
+        
+        // Выходные данные
+        evbuffer_add_printf(outBuf, "<html><body><center><h1>Hello Wotld!</h1></center></body></html>");
+        
+        // отвечаем на запрос
+        evhttp_send_reply(req, HTTP_OK, "", outBuf);
     };
-    evhttp_set_gencb(Server.get(), OnReq, nullptr);
-    if (event_dispatch() == -1)
-    {
+    
+    // включаем обработчик вызовов
+    evhttp_set_gencb(server.get(), receivedRequest, nullptr);
+    
+    // ошибка цикла LibEvent
+    if (event_dispatch() == -1){
         std::cerr << "Failed to run messahe loop." << std::endl;
         return -1;
     }
     return 0;
 }
 
-int multithreadedServer()
-{
-    char const SrvAddress[] = "127.0.0.1";
-    std::uint16_t const SrvPort = 5555;
-    int const SrvThreadCount = 4;
+int multithreadedServer() {
+    char const serverAddress[] = "127.0.0.1";
+    std::uint16_t const serverPort = 5555;
+    int const threadsCount = 4;
+    
     try
     {
-        void (*OnRequest)(evhttp_request *, void *) = [] (evhttp_request *req, void *)
-        {
-            auto *OutBuf = evhttp_request_get_output_buffer(req);
-            if (!OutBuf)
+        // коллбек запроса
+        void (*receivedRequest)(evhttp_request *, void *) = [] (evhttp_request *req, void *) {
+            // выходной буффер запроса
+            auto *outBuf = evhttp_request_get_output_buffer(req);
+            if (!outBuf){
                 return;
-            evbuffer_add_printf(OutBuf, "<html><body><center><h1>Hello Wotld!</h1></center></body></html>");
-            evhttp_send_reply(req, HTTP_OK, "", OutBuf);
+            }
+            
+            // Выходные данные
+            evbuffer_add_printf(outBuf, "<html><body><center><h1>Hello Wotld!</h1></center></body></html>");
+            
+            // отвечаем на запрос
+            evhttp_send_reply(req, HTTP_OK, "", outBuf);
         };
-        std::exception_ptr InitExcept;
-        bool volatile IsRun = true;
-        evutil_socket_t Socket = -1;
-        auto ThreadFunc = [&] ()
-        {
-            try
-            {
-                std::unique_ptr<event_base, decltype(&event_base_free)> EventBase(event_base_new(), &event_base_free);
-                if (!EventBase)
+        
+        std::exception_ptr initException;
+        
+        bool volatile isRunning = true;
+        evutil_socket_t socket = -1;
+        
+        // Функция в потоке
+        auto ThreadFunc = [&] (){
+            try {
+                std::unique_ptr<event_base, decltype(&event_base_free)> eventBase(event_base_new(), &event_base_free);
+                if (!eventBase){
                     throw std::runtime_error("Failed to create new base_event.");
-                std::unique_ptr<evhttp, decltype(&evhttp_free)> EvHttp(evhttp_new(EventBase.get()), &evhttp_free);
-                if (!EvHttp)
+                }
+                
+                ServerPtr eventHttp(evhttp_new(eventBase.get()), &evhttp_free);
+                if (!eventHttp){
                     throw std::runtime_error("Failed to create new evhttp.");
-                evhttp_set_gencb(EvHttp.get(), OnRequest, nullptr);
-                if (Socket == -1)
-                {
-                    auto *BoundSock = evhttp_bind_socket_with_handle(EvHttp.get(), SrvAddress, SrvPort);
-                    if (!BoundSock)
+                }
+                
+                evhttp_set_gencb(eventHttp.get(), receivedRequest, nullptr);
+                
+                if (socket == -1){
+                    auto* BoundSock = evhttp_bind_socket_with_handle(eventHttp.get(), serverAddress, serverPort);
+                    if (!BoundSock){
                         throw std::runtime_error("Failed to bind server socket.");
-                    if ((Socket = evhttp_bound_socket_get_fd(BoundSock)) == -1)
+                    }
+                    
+                    // сокет создается
+                    if ((socket = evhttp_bound_socket_get_fd(BoundSock)) == -1){
                         throw std::runtime_error("Failed to get server socket for next instance.");
+                    }
                 }
                 else
                 {
-                    if (evhttp_accept_socket(EvHttp.get(), Socket) == -1)
+                    if (evhttp_accept_socket(eventHttp.get(), socket) == -1)
                         throw std::runtime_error("Failed to bind server socket for new instance.");
                 }
-                for ( ; IsRun ; )
-                {
-                    event_base_loop(EventBase.get(), EVLOOP_NONBLOCK);
+                
+                for ( ; isRunning ; ) {
+                    // запуск
+                    event_base_loop(eventBase.get(), EVLOOP_NONBLOCK);
+                    
                     std::this_thread::sleep_for(std::chrono::milliseconds(10));
                 }
             }
-            catch (...)
-            {
-                InitExcept = std::current_exception();
+            catch (...){
+                initException = std::current_exception();
             }
         };
-        auto ThreadDeleter = [&] (std::thread *t) { IsRun = false; t->join(); delete t; };
-        typedef std::unique_ptr<std::thread, decltype(ThreadDeleter)> ThreadPtr;
+        
+        
+        // не дает завершиться потокам
+        auto threadDeleter = [&] (std::thread *t) {
+            isRunning = false;
+            t->join();
+            delete t;
+        };
+        
+        // указатель на поток + функция, вызываемая при уничтожении
+        typedef std::unique_ptr<std::thread, decltype(threadDeleter)> ThreadPtr;
+        // пулл потоков
         typedef std::vector<ThreadPtr> ThreadPool;
-        ThreadPool Threads;
-        for (int i = 0 ; i < SrvThreadCount ; ++i)
-        {
-            ThreadPtr Thread(new std::thread(ThreadFunc), ThreadDeleter);
+        
+        
+        ThreadPool threads;
+        threads.reserve(threadsCount);
+        
+        for (int i = 0 ; i < threadsCount ; ++i) {
+            ThreadPtr Thread(new std::thread(ThreadFunc), threadDeleter);
+            
+            // задержка старта следующего потока
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            if (InitExcept != std::exception_ptr())
-            {
-                IsRun = false;
-                std::rethrow_exception(InitExcept);
+            
+            if (initException != std::exception_ptr()){
+                isRunning = false;
+                std::rethrow_exception(initException);
             }
-            Threads.push_back(std::move(Thread));
+            
+            // сохраняем поток
+            threads.push_back(std::move(Thread));
         }
+        
         std::cout << "Press Enter fot quit." << std::endl;
         std::cin.get();
-        IsRun = false;
+        
+        isRunning = false;
     }
-    catch (std::exception const &e)
-    {
+    catch (std::exception const &e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     return 0;
@@ -124,7 +178,7 @@ int multithreadedServer()
 
 int main()
 {
-    int result = simpleOneThreadServer();
+    int result = multithreadedServer();
 
     return result;
 }
