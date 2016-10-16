179db07a9345aedb0e37df622820b8e91a805388
Pavel Ershov <devnulpavel@gmail.com>
2016-10-16 17:39:45 +0300

    TCP server example

--- main.cpp
+++ main.cpp
@@ -6,12 +6,20 @@
 #include <cstdint>
 #include <vector>
 // libevent
+#include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/event.h>
+#include <event.h>
 #include <evhttp.h>
 
-// пример
-// https://habrahabr.ru/post/217437/
 
-using namespace std;
+// примеры
+// https://habrahabr.ru/post/217437/
+// http://incpp.blogspot.ru/2009/04/libevent.html
+// https://www.ibm.com/developerworks/ru/library/l-Libevent1/
+
+//using namespace std;
 
 typedef std::unique_ptr<event_base, decltype(&event_base_free)>  EventHandler;
 typedef std::unique_ptr<evhttp, decltype(&evhttp_free)> ServerPtr;
@@ -192,9 +200,99 @@ int multithreadedServer() {
 }
 
 
+int tcpServer() {
+    int port = 5555;
+    if(port < 0 || port > 65535 ){
+        fprintf( stderr, "Задан некорректный номер порта.\n" );
+        return -1;
+    }
+    
+    event_base* base = event_base_new();
+    if(!base ){
+        fprintf( stderr, "Ошибка при создании объекта event_base.\n" );
+        return -1;
+    }
+    
+    // адресс
+    sockaddr_in sin;
+    memset( &sin, 0, sizeof(sin) );
+    sin.sin_family = AF_INET;    /* работа с доменом IP-адресов */
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* принимать запросы с любых адресов */
+    sin.sin_port = htons(port);
+    
+    
+    // обработка принятия соединения
+    auto accept_connection_cb = [](evconnlistener *listener,
+                                   evutil_socket_t fd, sockaddr *addr, int sock_len,
+                                   void* arg) {
+        
+        // При обработке запроса нового соединения необходимо создать для него объект bufferevent
+        event_base* base = evconnlistener_get_base(listener);
+        bufferevent* buf_ev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE );
+        
+        // Функция обратного вызова для события: данные готовы для чтения в buf_ev
+        auto echo_read_cb = [](bufferevent *buf_ev, void *arg) {
+            
+            evbuffer* buf_input = bufferevent_get_input(buf_ev);
+            evbuffer* buf_output = bufferevent_get_output(buf_ev);
+            
+            // Данные просто копируются из буфера ввода в буфер вывода
+            evbuffer_add_printf(buf_output, "You write: ");
+            evbuffer_add_buffer(buf_output, buf_input);
+        };
+        
+        // Функция обратного вызова для события: данные готовы для чтения в buf_ev
+        auto echo_write_cb = [](bufferevent *buf_ev, void *arg) {
+            
+        };
+        
+        // коллбек обработки ивента
+        auto echo_event_cb = []( struct bufferevent *buf_ev, short events, void *arg){
+            if( events & BEV_EVENT_ERROR ){
+                perror( "Ошибка объекта bufferevent" );
+            }
+            if( events & (BEV_EVENT_EOF | BEV_EVENT_ERROR) ){
+                bufferevent_free( buf_ev );
+            }
+        };
+        
+        bufferevent_setcb(buf_ev, echo_read_cb, echo_write_cb, echo_event_cb, NULL);
+        bufferevent_enable(buf_ev, (EV_READ | EV_WRITE) );
+    };
+
+    // ошибка в принятии соединения
+    auto accept_error_cb = []( struct evconnlistener *listener, void *arg){
+        struct event_base *base = evconnlistener_get_base( listener );
+        int error = EVUTIL_SOCKET_ERROR();
+        fprintf( stderr, "Ошибка %d (%s) в мониторе соединений. Завершение работы.\n",
+                error, evutil_socket_error_to_string( error ) );
+        event_base_loopexit( base, NULL );
+    };
+
+    
+    // лиснер
+    evconnlistener* listener = evconnlistener_new_bind(base, accept_connection_cb, NULL,
+                                       (LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE),
+                                       -1, (sockaddr*)&sin, sizeof(sin));
+    // проверка ошибки
+    if( !listener ){
+        perror( "Ошибка при создании объекта evconnlistener" );
+        return -1;
+    }
+    
+    // обработчик ошибки
+    evconnlistener_set_error_cb(listener, accept_error_cb );
+    
+    // запуск обработки событий
+    event_base_dispatch(base);
+    
+    return 0;
+}
+
+
 int main()
 {
-    int result = multithreadedServer();
+    int result = tcpServer();
 
     return result;
 }
